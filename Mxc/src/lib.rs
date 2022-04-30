b7e9a628fc36ae2040ff5140e8a444b301cea255
mahmudsudo <blinkztyler@gmail.com>
2022-04-30 02:06:37 +0100

    gnb

--- Mxc/src/lib.rs
+++ Mxc/src/lib.rs
@@ -32,12 +32,9 @@ pub mod pallet {
 	#[pallet::storage]
 	#[pallet::getter(fn something)]
 	
-	pub type Something<T> = StorageValue<_, List<T : Config + parity_scale_codec::Encode>>;
-
-	#[derive(codec::Encode,scale_info::TypeInfo,Decode)]
-	  pub  struct List<T:Config> {
-		  AccountId : Vec<T>
-	  }
+	pub type Something<T> = StorageValue<_, list>
+	
+	pub let mut list: Vec<T::AccountId>
 	
 
 	// Pallets use events to inform users when important changes are made.
@@ -64,45 +61,58 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		
-		#[pallet::weight(10000)]
-        pub fn add_user(origin: OriginFor<T>,new : T) -> DispatchResult {
-            ensure_root(origin)?;
-			//get the vector and the value
-			// <Something<T>>::put(new);
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					//add new user
-					old.AccountId.push(new);
-					// Update the value in storage with the incremented result.
-					
-					Ok(())
-				},
-			}
-            Ok(())
-        }
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			<Something<T>>::put(something);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored(something, who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
 		#[pallet::weight(0)]
-        pub fn remove_user(origin: OriginFor<T>,user: T) -> DispatchResult {
+        pub fn add_user(origin: OriginFor<T>) -> DispatchResult {
             ensure_root(origin)?;
-			//get the vector and the value
-			// <Something<T>>::put(new);
+
 			match <Something<T>>::get() {
 				// Return an error if the value has not been set.
 				None => Err(Error::<T>::NoneValue)?,
 				Some(old) => {
-					//remove user
-					old.remove(user);
+					// Increment the value read from storage; will error in the event of overflow.
+					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
 					// Update the value in storage with the incremented result.
-					
+					<Something<T>>::put(new);
 					Ok(())
 				},
 			}
             Ok(())
         }
 
+		/// An example dispatchable that may throw a custom error.
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
+		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
+			let _who = ensure_signed(origin)?;
 
-		
+			// Read a value from storage.
+			match <Something<T>>::get() {
+				// Return an error if the value has not been set.
+				None => Err(Error::<T>::NoneValue)?,
+				Some(old) => {
+					// Increment the value read from storage; will error in the event of overflow.
+					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					// Update the value in storage with the incremented result.
+					<Something<T>>::put(new);
+					Ok(())
+				},
+			}
+		}
 	}
 }
